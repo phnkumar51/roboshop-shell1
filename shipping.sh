3b59cd58df5132d50cb920118851ae145b4b81b7
Balaphani <phnkumar51@gmail.com>
2025-05-28 18:24:39 +0100

    shipping

--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,8 @@
 dnf install maven -y
 useradd roboshop
 
+cp shipping.service /etc/systemd/system/shipping.service
+
 rm -rf /app
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
@@ -11,3 +13,15 @@ cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
+
+dnf install mysql -y
+
+mysql -h mysql-dev.bdevops.online -uroot -pRoboShop@1 < /app/db/schema.sql
+
+mysql -h mysql-dev.bdevops.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+
+mysql -h mysql-dev.bdevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+systemctl daemon-reload
+systemctl enable shipping
+systemctl restart shipping
