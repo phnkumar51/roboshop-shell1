99102d7cdaa6bb7df41869a64b305331b6d06152
Balaphani <phnkumar51@gmail.com>
2025-05-28 18:14:04 +0100

    mysql component added

---
+++ shipping.sh
@@ -0,0 +1,13 @@
+dnf install maven -y
+useradd roboshop
+
+rm -rf /app
+mkdir /app
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
+cd /app
+unzip /tmp/shipping.zip
+
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
