de331fe34e319da150bd6ecf01957fb5fcebe13a
Balaphani <phnkumar51@gmail.com>
2025-05-30 15:29:43 +0100

    Update all these files

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,9 @@
 component=catalogue
 source common.sh
 
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-
-dnf install nodejs -y
-
-cp catalogue.service /etc/systemd/system/catalogue.service
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+nodejs
 
-
-useradd roboshop
-
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-
-cd /app
-npm install
-
-systemd_setup
 
 dnf install mongodb-mongosh -y
 mongosh --host mongo-dev.bdevops.online </app/db/master-data.js
