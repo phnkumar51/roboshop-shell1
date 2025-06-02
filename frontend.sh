a05b461642203ff3ef0bf7201cb4b519ae42b1e0
Balaphani <phnkumar51@gmail.com>
2025-06-02 10:41:26 +0530

    Added colour coding

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,30 @@
-echo disable nginx
+# To print the colour follow the below syntax
+# echo -e"\e[COLmxxxxx\e[0m
+
+
+echo -e "\e[31mDisable Nginx\e[0m"
 dnf module disable nginx -y
 
-echo enable nginx
+
+echo -e "\e[32mEnable Nginx\e[0m"
 dnf module enable nginx:1.24 -y
 
-echo install nginx
+echo -e "\e[33mInstall Nginx\e[0m"
 dnf install nginx -y
 
-echo copy nginx configuration file
+echo -e "\e[34mcopy Nginx Conf file\e[0m"
 cp nginx.conf /etc/nginx/nginx.conf
 
-echo remove old content on nginx
+echo -e "\e[35mremove the old content in Nginx\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo download the content of nginx web server
+echo -e "\e[35mDownload Nginx content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 
-echo extract app content
+echo -e "\e[35mDisable Nginx\e[0m"
 unzip /tmp/frontend.zip
 
-echo restart nginx
+echo -e"\e[31Restart Nginx\e[0m"
 systemctl enable nginx
 systemctl restart nginx
