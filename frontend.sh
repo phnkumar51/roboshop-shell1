abffe3cee5b2483dd0f6fffc98ee0177ddbe9411
Balaphani <phnkumar51@gmail.com>
2025-06-03 13:45:16 +0530

    Added colour coding

--- frontend.sh
+++ frontend.sh
@@ -3,39 +3,39 @@
 
 print_head(){
   echo -e "\e[36m$*\e[0m"
-  echo "################################" >> log_file
+  echo "################################" &>> log_file
   echo -e "\e[36m$*\e[0m" >> log_file
-  echo "################################" >> log_file
+  echo "################################" &>> log_file
 }
 
 log_file=/tmp/roboshop.log
 rm -f $log_file
 print_head Disable Default Nginx
-dnf module disable nginx -y $&>> $log_file
+dnf module disable nginx -y &>> $log_file
 
 
 print_head Enable Nginx
-dnf module enable nginx:1.24 -y $&>> $log_file
+dnf module enable nginx:1.24 -y &>> $log_file
 
 print_head Install Nginx
-dnf install nginx -y $&>> $log_file
+dnf install nginx -y &>> $log_file
 
 print_head copy Nginx Conf file
-cp nginx.conf /etc/nginx/nginx.conf $&>> $log_file
+cp nginx.conf /etc/nginx/nginx.conf &>> $log_file
 
 print_head remove the old content in Nginx
-rm -rf /usr/share/nginx/html/* $&>> $log_file
+rm -rf /usr/share/nginx/html/* &>> $log_file
 
 print_head Download Nginx content
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip $&>> $log_file
-cd /usr/share/nginx/html >>/tmp/roboshop.log $&>> $log_file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $log_file
+cd /usr/share/nginx/html >>/tmp/roboshop.log &>> $log_file
 
-print_head Disable Nginx
-unzip /tmp/frontend.zip $&>> $log_file
+print_head Extract Nginx
+unzip /tmp/frontend.zip &>> $log_file
 
 print_head Restart Nginx\e
-systemctl enable nginx $&>> $log_file
-systemctl restart nginx $&>> $log_file
+systemctl enable nginx &>> $log_file
+systemctl restart nginx &>> $log_file
 
 # environmental variables
 # Input variables script Input variables
